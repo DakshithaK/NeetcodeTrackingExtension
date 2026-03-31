08a65d2045baad2be7568ad500e45cafc7c35a1b
Dakshitha K <140424082+DakshithaK@users.noreply.github.com>
2026-03-31 20:42:54 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    github: {
+        username: "your-github-username",
+        repo_name: "your-repository-name",
+        token: "your-github-personal-access-token",
+        committer_name: "Your Name",
+        committer_email: "your-email@example.com"
+    }
+};
